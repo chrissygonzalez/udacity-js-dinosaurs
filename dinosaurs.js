d026f7a0f23c8e34899885b4992593b21c7f5707
Chrissy Gonzalez <chrissy.gonzalez@electric.ai>
2021-02-14 09:24:42 -0500

    feat: add simple Human class, create on submit

--- dinosaurs.js
+++ dinosaurs.js
@@ -2,8 +2,32 @@ const dinoSubmit = document.getElementById('dino-submit');
 const dinoForm = document.getElementById('dino-form');
 const dinoGraphic = document.getElementById('dino-graphic');
 
+const nameField = document.getElementById('name');
+const feetField = document.getElementById('feet');
+const inchesField = document.getElementById('inches');
+const poundsField = document.getElementById('pounds');
+const dietField = document.getElementById('diet');
+
+class Human {
+  constructor({ name, feet, inches, pounds, diet }) {
+    this.name = name;
+    this.feet = feet;
+    this.inches = inches;
+    this.pounds = pounds;
+    this.diet = diet;
+  }
+}
+
 const submitForm = (e) => {
   e.preventDefault();
+  const human = new Human({
+    name: nameField.value,
+    feet: feetField.value,
+    inches: inchesField.value,
+    pounds: poundsField.value,
+    diet: dietField.value,
+  });
+  console.log(human);
   dinoForm.classList.add('invisible');
   dinoGraphic.classList.remove('invisible');
 };
